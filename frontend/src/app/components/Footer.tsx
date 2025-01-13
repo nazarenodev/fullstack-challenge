09193b771beba7d07771edf0fe2f641297badb27
nazarenodev <nazarenodeveloper@gmail.com>
2025-01-13 10:35:43 +0000

    Frontend - Defining basic layout components

---
+++ frontend/src/app/components/Footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+    <footer className="bg-gray-800 text-white p-4 text-center">
+        <p>&copy; {new Date().getFullYear()} Shoe Store - Fullstack Challenge</p>
+    </footer>
+}
---
+++ frontend/src/app/components/ShoeCard.tsx
@@ -0,0 +1,14 @@
+interface Shoe {
+    id: string;
+    brand: string;
+    availableSizes: number[];
+    price: number;
+}
+
+export default function ShoeCard(shoe: Shoe) {
+    <div className="border p-4 rounded shadow-md">
+        <h3 className="text-lg font-bold">{shoe.brand}</h3>
+        <p>Price: ${shoe.price}</p>
+        <p>Available Sizes: {shoe.availableSizes.join(', ')}</p>
+    </div>
+};
